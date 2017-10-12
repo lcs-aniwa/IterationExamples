4e1d2cf71018952bde6a857fce283bb552ae6c59
Niwa Ayana <aniwa@lcs-mba3092.lcs.on.ca>
2017-10-12 14:09:19 -0400

    I made the cross. (slash line)

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -19,10 +19,13 @@ for x in stride(from: 25, through: 275, by: 50) {
     for y in stride(from: 275,through: 25, by: -50) {
         y
         canvas.drawEllipse(centreX: x, centreY: y, width: 10, height: 10)
-        
+        canvas.drawLine(fromX: x-25, fromY: y-25, toX: x+25, toY: y+25)
+        canvas.drawLine(fromX: x-25, fromY: y+25, toX: x+25, toY: y-25)
     }
 
 }
+
+
 
 
 
